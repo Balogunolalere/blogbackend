63b6df704d6ccd29072f670a76c3bb62056e4963
Balogunolalere <lordareello@gmail.com>
2025-02-01 04:26:42 +0100

    Remove deprecated cron job implementation and related Vercel configuration

--- api/index.py
+++
@@ -1,9 +0,0 @@
-
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from main import app
-
-# This is required for Vercel deployment
-handler = app
